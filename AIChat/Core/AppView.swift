644a31936aff935f386fd2695a4e7f93b037340a
Dylan Ierugan <dylanierugan@gmail.com>
2025-04-08 23:42:00 -0500

    [Feature] Add AppView and AppViewBuilder]

---
+++ AIChat/Core/AppView.swift
@@ -0,0 +1,57 @@
+//
+//  AppView.swift
+//  AIChat
+//
+//  Created by Dylan Ierugan on 4/8/25.
+//
+
+import SwiftUI
+
+struct AppViewBuilder<TabbarView: View, OnboardingView: View>: View {
+    
+    var showTabBar: Bool = false
+    @ViewBuilder var tabbarView: TabbarView
+    @ViewBuilder var onboardingView: OnboardingView
+    
+    var body: some View {
+        ZStack {
+            if showTabBar {
+                tabbarView
+                    .transition(.move(edge: .trailing))
+            } else {
+                onboardingView
+                    .transition(.move(edge: .leading))
+            }
+        }
+        .animation(.smooth, value: showTabBar)
+    }
+}
+
+struct AppView: View {
+    
+    @State private var showTabBar: Bool = false
+    
+    var body: some View {
+        AppViewBuilder(showTabBar: showTabBar,
+                       tabbarView: {
+            ZStack {
+                Color.red.ignoresSafeArea()
+                Text("Tab Bar")
+            }
+            .transition(.move(edge: .trailing))
+        }, onboardingView: {
+            ZStack {
+                Color.blue.ignoresSafeArea()
+                Text("Onboaring")
+            }
+            .transition(.move(edge: .leading))
+        })
+        .onTapGesture {
+            showTabBar.toggle()
+        }
+    }
+}
+
+#Preview {
+    AppView()
+}
