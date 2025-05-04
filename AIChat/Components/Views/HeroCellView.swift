2c08f732fcd4aa358e47ad0f3a98f136bfcb31c4
Dylan Ierugan <dylanierugan@gmail.com>
2025-05-04 00:33:21 -0500

    [Feature] HeroCellView

---
+++ AIChat/Components/Views/HeroCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCellView.swift
+//  AIChat
+//
+//  Created by Dylan Ierugan on 5/4/25.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroCellView()
+}
