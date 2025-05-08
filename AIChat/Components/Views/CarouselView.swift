dfa1e977461493443fbded308847736c27c54e56
Dylan Ierugan <dylanierugan@gmail.com>
2025-05-07 21:15:04 -0500

    [Feature] CarouselView

---
+++ AIChat/Components/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  AIChat
+//
+//  Created by Dylan Ierugan on 5/7/25.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
