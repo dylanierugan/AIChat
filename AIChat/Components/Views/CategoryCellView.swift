40cdd424077837e288971a45845e96297a4eae62
Dylan Ierugan <dylanierugan@gmail.com>
2025-05-18 00:40:09 -0500

    [Feature] CategoryCell

---
+++ AIChat/Components/Views/CategoryCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCellView.swift
+//  AIChat
+//
+//  Created by Dylan Ierugan on 5/18/25.
+//
+
+import SwiftUI
+
+struct CategoryCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCellView()
+}
